31aebd7c94bc55bcd34d3dab314df70aadec859b
Alicia Alexia <alicia.alexia03@gmail.com>
2023-06-07 20:13:20 -0300

    starting project creation of component, service and class tasks

---
+++ src/app/tasks/task-list-form/task-list-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list-form',
+  templateUrl: './task-list-form.component.html',
+  styleUrls: ['./task-list-form.component.css']
+})
+export class TaskListFormComponent {
+
+}
