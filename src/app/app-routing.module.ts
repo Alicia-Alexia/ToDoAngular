4e751890b19c607c796c0a608910a111ff92ddd2
Alicia Alexia <alicia.alexia03@gmail.com>
2023-06-07 22:11:03 -0300

    creation of activity list structure, change and delete buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { TaskListComponent } from './tasks/task-list/task-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListFormComponent } from './tasks/task-form/task-list-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: TaskListComponent},
+  { path: 'new', component: TaskListFormComponent},
+  { path: 'edit/:id', component: TaskListFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
